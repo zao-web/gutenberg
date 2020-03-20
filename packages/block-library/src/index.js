3cfe60d2692c269289e75ff0cf5388b5d1ed1200
Sean Leavey <gpg@attackllama.com>
2020-03-20 10:46:54 +0100

    SeanDS's implementation #21040 (https://github.com/WordPress/gutenberg/pull/21040).
    Copy changes from pull request #15426 (https://github.com/WordPress/gutenberg/pull/15426).
    
    Adds Table of Contents block to the editor.
    
    Code contributions in this commit entirely made by ashwin-pc, originally based on the
    "Guidepost" block by sorta brilliant (https://sortabrilliant.com/guidepost/).
    
    Apply polish suggestions from code review.
    
    Improve variable names.
    
    Add comment
    
    Get rid of autosync (users should now convert to list if they want to edit the contents)
    
    Add ability to transform into list; remove unused ListLevel props
    
    Update table-of-contents block test configuration
    
    Simplify expression
    
    Remove unused function
    
    Remove unused styles.
    
    Rename TOCEdit to TableOfContentsEdit
    
    Apply suggestions from code review
    
    Remove non-existent import
    
    Make imports explicit
    
    Remove unused function
    
    Change unsubscribe function to class property
    
    Change JSON.stringify comparison to Lodash's isEqual
    
    Turns out refresh() is required
    
    Remove unnecessary state setting
    
    Don't change state on save
    
    Change behaviour to only add links if there are anchors specified by the user
    
    Newline
    
    Replace anchor with explicit key in map since anchor can now sometimes be empty
    
    Update test data
    
    Update packages/block-library/src/table-of-contents/block.json
    
    Rename ListLevel to ListItem for clarity and polish.
    
    Co-authored-by: ashwin-pc <ashwinpc1993@gmail.com>
    Co-authored-by: Daniel Richards <daniel.p.richards@gmail.com>
    Co-authored-by: Zebulan Stanphill <zebulanstanphill@protonmail.com>

--- packages/block-library/src/index.js
+++ packages/block-library/src/index.js
@@ -53,6 +53,7 @@ import * as shortcode from './shortcode';
 import * as spacer from './spacer';
 import * as subhead from './subhead';
 import * as table from './table';
+import * as tableOfContents from './table-of-contents';
 import * as textColumns from './text-columns';
 import * as verse from './verse';
 import * as video from './video';
@@ -156,6 +157,7 @@ export const __experimentalGetCoreBlocks = () => [
 	spacer,
 	subhead,
 	table,
+	tableOfContents,
 	tagCloud,
 	textColumns,
 	verse,
