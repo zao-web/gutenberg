5d70e0c7c41c770cef081c9317caad168bbcbc25
Zebulan Stanphill <zebulanstanphill@protonmail.com>
2020-05-20 15:03:58 -0500

    Add support for paginated posts.

--- packages/block-library/src/table-of-contents/edit.js
+++ packages/block-library/src/table-of-contents/edit.js
@@ -6,8 +6,12 @@ import { isEqual } from 'lodash';
 /**
  * WordPress dependencies
  */
-import { BlockIcon, useBlockProps } from '@wordpress/block-editor';
-import { Placeholder } from '@wordpress/components';
+import {
+	BlockIcon,
+	InspectorControls,
+	useBlockProps,
+} from '@wordpress/block-editor';
+import { PanelBody, Placeholder, ToggleControl } from '@wordpress/components';
 import { useSelect } from '@wordpress/data';
 import { useEffect, useState } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
@@ -16,12 +20,26 @@ import { __ } from '@wordpress/i18n';
  * Internal dependencies
  */
 import TableOfContentsList from './list';
-import {
-	getHeadingsFromHeadingElements,
-	linearToNestedHeadingList,
-} from './utils';
+import { getHeadingsFromContent, linearToNestedHeadingList } from './utils';
 
-export default function TableOfContentsEdit() {
+/**
+ * Table of Contents block edit component.
+ *
+ * @param {Object}   props            The props.
+ * @param {Object}   props.attributes The block attributes.
+ * @param {boolean}  props.attributes.onlyIncludeCurrentPage
+ * Whether to only include headings from the current page (if the post is
+ * paginated).
+ * @param {string}   props.clientId
+ * @param {Function} props.setAttributes
+ *
+ * @return {WPComponent} The component.
+ */
+export default function TableOfContentsEdit( {
+	attributes: { onlyIncludeCurrentPage },
+	clientId,
+	setAttributes,
+} ) {
 	const blockProps = useBlockProps();
 
 	// Local state; not saved to block attributes. The saved block is dynamic and uses PHP to generate its content.
@@ -31,38 +49,79 @@ export default function TableOfContentsEdit() {
 		return select( 'core/editor' ).getEditedPostContent();
 	}, [] );
 
+	const pageIndex = useSelect(
+		( select ) => {
+			const { getBlockIndex, getBlockName, getBlockOrder } = select(
+				'core/block-editor'
+			);
+
+			const blockIndex = getBlockIndex( clientId );
+			const blockOrder = getBlockOrder();
+
+			// Calculate which page the block will appear in on the front-end by
+			// counting how many core/nextpage blocks precede it.
+			// Unfortunately, this does not account for <!--nextpage--> tags in
+			// other blocks, so in certain edge cases, this will calculate the
+			// wrong page number. Thankfully, this issue only affects the editor
+			// implementation.
+			let page = 1;
+			for ( let i = 0; i < blockIndex; i++ ) {
+				if ( getBlockName( blockOrder[ i ] ) === 'core/nextpage' ) {
+					page++;
+				}
+			}
+
+			return page;
+		},
+		[ clientId ]
+	);
+
 	useEffect( () => {
-		// Create a temporary container to put the post content into, so we can
-		// use the DOM to find all the headings.
-		const tempPostContentDOM = document.createElement( 'div' );
-		tempPostContentDOM.innerHTML = postContent;
+		let latestHeadings;
 
-		// Remove template elements so that headings inside them aren't counted.
-		// This is only needed for IE11, which doesn't recognize the element and
-		// treats it like a div.
-		for ( const template of tempPostContentDOM.querySelectorAll(
-			'template'
-		) ) {
-			tempPostContentDOM.removeChild( template );
+		if ( onlyIncludeCurrentPage ) {
+			const pagesOfContent = postContent.split( '<!--nextpage-->' );
+
+			latestHeadings = getHeadingsFromContent(
+				pagesOfContent[ pageIndex - 1 ]
+			);
+		} else {
+			latestHeadings = getHeadingsFromContent( postContent );
 		}
-
-		const headingElements = tempPostContentDOM.querySelectorAll(
-			'h1, h2, h3, h4, h5, h6'
-		);
-
-		const latestHeadings = getHeadingsFromHeadingElements(
-			headingElements
-		);
 
 		if ( ! isEqual( headings, latestHeadings ) ) {
 			setHeadings( latestHeadings );
 		}
-	}, [ postContent ] );
+	}, [ pageIndex, postContent, onlyIncludeCurrentPage ] );
+
+	const inspectorControls = (
+		<InspectorControls>
+			<PanelBody title={ __( 'Table of Contents settings' ) }>
+				<ToggleControl
+					label={ __( 'Only include current page' ) }
+					checked={ onlyIncludeCurrentPage }
+					onChange={ ( value ) =>
+						setAttributes( { onlyIncludeCurrentPage: value } )
+					}
+					help={
+						onlyIncludeCurrentPage
+							? __(
+									'Only including headings from the current page (if the post is paginated).'
+							  )
+							: __(
+									'Toggle to only include headings from the current page (if the post is paginated).'
+							  )
+					}
+				/>
+			</PanelBody>
+		</InspectorControls>
+	);
 
 	// If there are no headings or the only heading is empty.
 	if ( headings.length === 0 || headings[ 0 ].content === '' ) {
 		return (
 			<div { ...blockProps }>
+				{ inspectorControls }
 				<Placeholder
 					className="wp-block-table-of-contents"
 					icon={ <BlockIcon icon="list-view" /> }
@@ -77,6 +136,7 @@ export default function TableOfContentsEdit() {
 
 	return (
 		<nav { ...blockProps }>
+			{ inspectorControls }
 			<TableOfContentsList
 				nestedHeadingList={ linearToNestedHeadingList( headings ) }
 			/>
