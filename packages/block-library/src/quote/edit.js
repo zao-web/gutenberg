cfc9185a6f4cdc9ce70162b202846fb21e9bf1a2
Andrés <andres.maneiro@automattic.com>
2020-10-13 11:09:36 +0200

    Make edit markup look like save (use lightblockwrapper for inner blocks)

--- packages/block-library/src/quote/edit.js
+++ packages/block-library/src/quote/edit.js
@@ -6,46 +6,22 @@ import classnames from 'classnames';
 /**
  * WordPress dependencies
  */
-import { __ } from '@wordpress/i18n';
 import {
 	AlignmentToolbar,
 	BlockControls,
-	InnerBlocks,
-	RichText,
 	useBlockProps,
+	__experimentalUseInnerBlocksProps as useInnerBlocksProps,
 } from '@wordpress/block-editor';
 import { BlockQuotation } from '@wordpress/components';
-import { createBlock } from '@wordpress/blocks';
-import { useSelect } from '@wordpress/data';
 
-export default function QuoteEdit( {
-	attributes,
-	setAttributes,
-	isSelected,
-	className,
-	insertBlocksAfter,
-	clientId,
-} ) {
-	const { align, citation } = attributes;
+export default function QuoteEdit( { attributes, setAttributes, className } ) {
+	const { align } = attributes;
 	const blockProps = useBlockProps( {
 		className: classnames( className, {
 			[ `has-text-align-${ align }` ]: align,
 		} ),
 	} );
-
-	const { isAncestorOfSelectedBlock } = useSelect(
-		( select ) => {
-			const { hasSelectedInnerBlock } = select( 'core/block-editor' );
-
-			return {
-				isAncestorOfSelectedBlock: hasSelectedInnerBlock(
-					clientId,
-					true
-				),
-			};
-		},
-		[ clientId ]
-	);
+	const innerBlocksProps = useInnerBlocksProps( blockProps );
 
 	return (
 		<>
@@ -57,39 +33,7 @@ export default function QuoteEdit( {
 					} }
 				/>
 			</BlockControls>
-			<BlockQuotation { ...blockProps }>
-				<InnerBlocks
-					allowedBlocks={ [
-						'core/code',
-						'core/heading',
-						'core/list',
-						'core/paragraph',
-					] }
-				/>
-				{ ( ! RichText.isEmpty( citation ) ||
-					isSelected ||
-					isAncestorOfSelectedBlock ) && (
-					<RichText
-						identifier="citation"
-						value={ citation }
-						onChange={ ( nextCitation ) =>
-							setAttributes( {
-								citation: nextCitation,
-							} )
-						}
-						__unstableMobileNoFocusOnMount
-						placeholder={
-							// translators: placeholder text used for the citation
-							__( 'Write citation…' )
-						}
-						className="wp-block-quote__citation"
-						textAlign={ align }
-						__unstableOnSplitAtEnd={ () =>
-							insertBlocksAfter( createBlock( 'core/paragraph' ) )
-						}
-					/>
-				) }
-			</BlockQuotation>
+			<BlockQuotation { ...innerBlocksProps } />
 		</>
 	);
 }
