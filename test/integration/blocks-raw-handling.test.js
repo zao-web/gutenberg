647bf682b2f188a84f4d7e587458ace6809d97be
Andrés <andres.maneiro@automattic.com>
2020-10-15 18:33:17 +0200

    block-raw-handling-test: citation in the middle of content

--- test/integration/blocks-raw-handling.test.js
+++ test/integration/blocks-raw-handling.test.js
@@ -306,7 +306,13 @@ describe( 'Blocks raw handling', () => {
 			.join( '' );
 
 		expect( filtered ).toBe(
-			'<blockquote class="wp-block-quote"><p>chicken</p><p>ribs</p><cite>ribs</cite></blockquote>'
+			`<blockquote class="wp-block-quote"><!-- wp:paragraph -->
+<p>chicken</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>ribs</p>
+<!-- /wp:paragraph --><cite>ribs</cite></blockquote>`
 		);
 		expect( console ).toHaveLogged();
 	} );
