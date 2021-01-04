016aef68a5f81adfc14bf9484234a69c00a941c1
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2021-01-04 18:02:55 +0100

    Revert changes related to webpack 5 upgrade

--- packages/scripts/config/webpack.config.js
+++ packages/scripts/config/webpack.config.js
@@ -2,6 +2,7 @@
  * External dependencies
  */
 const { BundleAnalyzerPlugin } = require( 'webpack-bundle-analyzer' );
+const LiveReloadPlugin = require( 'webpack-livereload-plugin' );
 const MiniCSSExtractPlugin = require( 'mini-css-extract-plugin' );
 const TerserPlugin = require( 'terser-webpack-plugin' );
 const { CleanWebpackPlugin } = require( 'clean-webpack-plugin' );
@@ -17,6 +18,7 @@ const postcssPlugins = require( '@wordpress/postcss-plugins-preset' );
  * Internal dependencies
  */
 const { hasBabelConfig, hasPostCSSConfig } = require( '../utils' );
+const FixStyleWebpackPlugin = require( './fix-style-webpack-plugin' );
 
 const isProduction = process.env.NODE_ENV === 'production';
 const mode = isProduction ? 'production' : 'development';
@@ -97,25 +99,31 @@ const config = {
 			{
 				test: /\.js$/,
 				exclude: /node_modules/,
-				loader: require.resolve( 'babel-loader' ),
-				options: {
-					// Babel uses a directory within local node_modules
-					// by default. Use the environment variable option
-					// to enable more persistent caching.
-					cacheDirectory: process.env.BABEL_CACHE_DIRECTORY || true,
+				use: [
+					require.resolve( 'thread-loader' ),
+					{
+						loader: require.resolve( 'babel-loader' ),
+						options: {
+							// Babel uses a directory within local node_modules
+							// by default. Use the environment variable option
+							// to enable more persistent caching.
+							cacheDirectory:
+								process.env.BABEL_CACHE_DIRECTORY || true,
 
-					// Provide a fallback configuration if there's not
-					// one explicitly available in the project.
-					...( ! hasBabelConfig() && {
-						babelrc: false,
-						configFile: false,
-						presets: [
-							require.resolve(
-								'@wordpress/babel-preset-default'
-							),
-						],
-					} ),
-				},
+							// Provide a fallback configuration if there's not
+							// one explicitly available in the project.
+							...( ! hasBabelConfig() && {
+								babelrc: false,
+								configFile: false,
+								presets: [
+									require.resolve(
+										'@wordpress/babel-preset-default'
+									),
+								],
+							} ),
+						},
+					},
+				],
 			},
 			{
 				test: /\.svg$/,
@@ -147,7 +155,17 @@ const config = {
 		// bundle content as a convenient interactive zoomable treemap.
 		process.env.WP_BUNDLE_ANALYZER && new BundleAnalyzerPlugin(),
 		// MiniCSSExtractPlugin to extract the CSS thats gets imported into JavaScript.
-		new MiniCSSExtractPlugin( { filename: '[name].css' } ),
+		new MiniCSSExtractPlugin( { esModule: false, filename: '[name].css' } ),
+		// MiniCSSExtractPlugin creates JavaScript assets for CSS that are
+		// obsolete and should be removed. Related webpack issue:
+		// https://github.com/webpack-contrib/mini-css-extract-plugin/issues/85
+		new FixStyleWebpackPlugin(),
+		// WP_LIVE_RELOAD_PORT global variable changes port on which live reload
+		// works when running watch mode.
+		! isProduction &&
+			new LiveReloadPlugin( {
+				port: process.env.WP_LIVE_RELOAD_PORT || 35729,
+			} ),
 		// WP_NO_EXTERNALS global variable controls whether scripts' assets get
 		// generated, and the default externals set.
 		! process.env.WP_NO_EXTERNALS &&
